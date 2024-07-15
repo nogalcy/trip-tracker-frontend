931616c8b09b90cda8f76c23d7699cf005aef1ea
Cy Logan <clogan2004@gmail.com>
2024-07-15 00:01:44 -0400

    routing change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ const App = () => {
         <Route path='/profile' element={<Profile />}></Route>
         <Route path='/profile/favorites' element={<FavoriteTrips />}></Route>
         <Route path='/profile/user-information' element={<UserInfo />}></Route>
-        <Route path='/' element={<Navigate to='/register' />}></Route>
+        <Route path='/' element={<Navigate to='/login' />}></Route>
+        <Route path='*' element={<Navigate to='/register' />}></Route>
       </Routes>
     </BrowserRouter>
   );
